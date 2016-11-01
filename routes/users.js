dc3666ca58c0a486ded639dcc8b6d574068a0641
Nico Cucciniello <ncucciniello@gmail.com>
2016-10-31 22:59:25 -0400

    add more styles

--- routes/users.js
+++ routes/users.js
@@ -10,19 +10,12 @@ const { searchSongs,
 
 const usersRouter        = express.Router();
 
-/**
- * Creates a new user by handling the POST request from a form with action `/users`
- * It uses the createUser middleware from the user model
- */
+
 usersRouter.post('/', createUser, (req, res) => {
   res.redirect('/');
 });
 
-/**
- * Takes the user to its profile by handling any GET request to `/users/profile`
- * It redirects to /login when attempted to be reached by a non logged in user
- * It is "protected" by the authenticate middleware from the auth library
- */
+
 usersRouter.get('/profile', authenticate, (req, res) => {
   res.render('users/profile', {
     user: res.user,
